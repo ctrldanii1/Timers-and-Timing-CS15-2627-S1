baea26458806b3f775a8ad2bfc61b3a46b2e4b60
Danii <daniellaadegun@gmail.com>
2026-09-23 14:35:32 -0600

    Update extension.py

--- root/extension.py
+++ root/extension.py
@@ -4,15 +4,15 @@ import random
 best_time = None
 for _ in range(5):
    time.sleep(random.randrange(2,5))
-start_time = time.monotonic()
-input("GO!")
-end_time = time.monotonic()
-speed = end_time - start_time
-print(speed)
-if best_time is None:
-   best_time = speed
-if speed < best_time:
-   best_time = speed
+   start_time = time.monotonic()
+   input("GO!")
+   end_time = time.monotonic()
+   speed = end_time - start_time
+   print(speed)
+   if best_time is None:
+      best_time = speed
+   if speed < best_time:
+      best_time = speed
 print(f"Your fastest time was {best_time}")
 
 
